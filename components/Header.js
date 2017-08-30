6a0166297c60032efca046fc0430282516df9079
liuwill <liuwei_will@qq.com>
2017-08-30 20:18:38 +0800

    ADD: jquery for upload file

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,15 @@ function Header (props) {
   return (
     <Head>
       <title>{props.siteTitle}</title>
+      <meta charset='UTF-8' />
       <meta name='description' content={props.description} />
+      <meta name='author' content='liuwill,liuwill@live.com' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       {props.stylesheets && props.stylesheets.length > 0 && props.stylesheets.map((stylesheet, i) => {
         return <link key={i} rel='stylesheet' href={stylesheet} />
+      })}
+      {props.scripts && props.scripts.length > 0 && props.scripts.map((scriptFile, i) => {
+        return <script key={i} src={scriptFile} />
       })}
 
       <style>{`
@@ -27,7 +32,8 @@ function Header (props) {
 Header.propTypes = {
   siteTitle: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  stylesheets: PropTypes.array
+  stylesheets: PropTypes.array,
+  scripts: PropTypes.array
 }
 
 export default Header
--- config/base.js
+++ config/base.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteTitle: 'IP to Geo',
   description: 'Generate Geo db for find-ip-location',
-  stylesheets: ['static/css/common.css']
+  stylesheets: ['static/css/common.css'],
+  scripts: ['static/js/jquery.min.js']
 }
