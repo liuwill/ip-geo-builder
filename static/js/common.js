209ce4f2ed875e813c1417508fe7194768b64351
liuwill <liuwei_will@qq.com>
2017-09-01 15:02:36 +0800

    IMPROVE: ui interative for upload

--- static/js/common.js
+++ static/js/common.js
@@ -26,32 +26,51 @@ $(function () {
       cardElement.attr('title', filename)
       $('.status', cardElement).text(filename)
       $('.size', cardElement).text(fileSize)
+      $('.action-box', cardElement).addClass('selected')
     }
+    $('.action-box', cardElement).removeClass('error')
   })
 
   $('.geo-ip-form').submit(function (e) {
     $('.tip-box').text('your file transform is starting')
   })
 
-  $('.form-data.trigger').keyup(function (e) {
-    if (e.keyCode === 13) {
-      if ($('.form-data.upload[name="core"]').val() === '') {
-        return
-      } else if (!$('.form-data.upload[name="geo"]').val() === '') {
-        return
+  $('.form-data.trigger')
+    .keydown(function (e) {
+      if (e.keyCode === 13) {
+        if ($('.form-data.upload[name="core"]').val() === '') {
+          $('.action-item.upload[name="core"] .action-box').addClass('error')
+        }
+
+        if ($('.form-data.upload[name="geo"]').val() === '') {
+          $('.action-item.upload[name="geo"] .action-box').addClass('error')
+        }
       }
+    })
+    .keyup(function (e) {
+      if (e.keyCode === 13) {
+        $('.action-item.upload .action-box').removeClass('error')
+        if ($('.form-data.upload[name="core"]').val() === '') {
+          return
+        }
 
-      $('.geo-ip-form').submit()
-      return false
-    } else if (e.keyCode === 27) {
-      $(this).val('')
-      $('.form-data.upload').val('')
-      $('.action-item.upload .status').text('点击上传')
+        if ($('.form-data.upload[name="geo"]').val() === '') {
+          return
+        }
 
-      $('.action-item.upload .size').text('0')
-      $('.tip-box').text('choose your files and input filename, then type enter.')
-    }
-  })
+        $('.geo-ip-form').submit()
+        return false
+      } else if (e.keyCode === 27) {
+        $(this).val('')
+        $('.form-data.upload').val('')
+        $('.action-item.upload .status').text('点击上传')
+
+        $('.action-item.upload .size').text('0')
+        $('.action-item.upload .action-box').removeClass('selected').removeClass('error')
+
+        $('.tip-box').text('choose your files and input filename, then type enter.')
+      }
+    })
 
   console.log('Have a great day! 🍭🍺')
 })
